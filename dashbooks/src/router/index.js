4c71e4c73c5ecbd5f1829ed8ee111cc378cee116
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-05-12 13:16:56 +1200

    init

---
+++ dashbooks/src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import AboutView from '../views/AboutView.vue';
+import SettingsView from '../views/SettingsView.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: SettingsView
+  }
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+export default router
