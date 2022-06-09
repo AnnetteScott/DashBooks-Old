948ca24ada1ac35855ae31af9fa8f55cebe57cf8
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-06-09 21:54:32 +1200

    1.0.0 for real

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -187,7 +187,6 @@ export default {
                 }
             }
         }
-        userDict['records']['headingStates'] = [ 'month', 'date', 'type', 'account', 'category', 'item', 'payee', 'amount' ]
         this.checkForUpdates();
 	},
 	methods: {
