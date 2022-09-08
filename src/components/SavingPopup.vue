8ba7df6d96cbc76662a3e4d74010768374d9d03c
NotNatural21 <annettemorganscott@outlook.co.nz>
2022-09-08 21:25:17 +1200

    Update SavingPopup.vue

--- src/components/SavingPopup.vue
+++ src/components/SavingPopup.vue
@@ -19,8 +19,8 @@ export default {
 #saving_pop_up {
 	position: absolute;
 	z-index: 1000;
-	top: 6px;
-	left: 200px;
+	top: 55px;
+	left: 10px;
 	display: flex;
 	flex-flow: row nowrap;
 	justify-content: center;
@@ -28,10 +28,11 @@ export default {
 	gap: 10px;
 	width: 100px;
 	height: 40px;
-	color: white;
-	font-size: 16px;
+	color: black;
+	font-size: 20px;
 	opacity: 0;
 	transition: 0.5s ease opacity;
+    pointer-events: none;
 }
 #saving_pop_up .spinner {
 	position: relative;
@@ -50,5 +51,6 @@ export default {
 }
 #saving_pop_up p {
 	font-size: 1em;
+    user-select: none;
 }
 </style>
